3272b9936d4740b26406cfba777d1793c04911b6
Shova <sumayashova180@gmail.com>
2022-10-16 21:51:07 +0600

    first commit

---
+++ src/vucanteen/VuCanteen.java
@@ -0,0 +1,13 @@
+
+package vucanteen;
+
+public class VuCanteen {
+
+    
+    public static void main(String[] args) {
+        canteen VuCanteen = new canteen ();
+        VuCanteen.setVisible(true);
+                
+    }
+    
+}
